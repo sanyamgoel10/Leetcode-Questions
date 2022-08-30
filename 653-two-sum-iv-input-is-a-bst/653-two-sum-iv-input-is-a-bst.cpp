12249290deccce860f05eaa7ac7ea470c30a5a88
Sanyam Goel <57743195+sanyamgoel10@users.noreply.github.com>
2022-08-30 23:34:04 +0530

    Time: 62 ms (58.79%), Space: 36.6 MB (92.58%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -9,40 +9,73 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-    void inorder(TreeNode* root, vector<int>& ans){
-        if(root==NULL)
-            return;
-        inorder(root->left, ans);
-        ans.push_back(root->val);
-        inorder(root->right, ans);
-    }
-public:
-    bool findTarget(TreeNode* root, int k) {
-        //find the inorder of BST
-        //inorder of BST is always sorted
-        //use two pointers to find elements with sum=k
-        //if found, make flag as true
-        //else, return flag as false
-        
-        vector<int> in;
-        inorder(root, in);
-        
-        int l = 0;
-        int r = in.size()-1;
-        
-        bool flag = false;
-        while(l<r){
-            int sum = in[l]+in[r];
-            if(sum>k){
-                r--;
-            }
-            else if(sum<k){
-                l++;
+class BSTiterator{
+    stack<TreeNode*> st;
+    // reverse = true -> previous
+    // reverse = false -> next
+    bool reverse = true;
+    
+    void pushInStack(TreeNode* root){
+        while(root){
+            st.push(root);
+            if(reverse){
+                root = root->right;
             }
             else{
+                root = root->left;
+            }
+        }
+    }
+public:
+    BSTiterator(TreeNode* root, bool isReverse){
+        reverse = isReverse;    //declare the direction here only
+        pushInStack(root);
+    }
+    
+    int next(){
+        TreeNode* curr = st.top();
+        st.pop();
+        if(reverse){
+            pushInStack(curr->left);
+        }
+        else{
+            pushInStack(curr->right);
+        }
+        return curr->val;
+    }   
+};
+
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        //use the approach of bst iterator
+        //here use bst iterator for both next and previous element
+        //use next() to find next element for each element
+        //use prev() to find previous element of each element
+        //we may use a single bst iterator class to implement both next and prev
+        //just use a flag variable for that
+        
+        if(root==NULL){
+            return false;
+        }
+        
+        BSTiterator l(root, false);     //iterate from last to first in bst
+        BSTiterator r(root, true);      //iterate from first to last in bst
+        
+        int i = l.next();
+        int j = r.next();
+        
+        bool flag = false;
+        while(i<j){
+            if(i+j == k){
                 flag = true;
                 break;
+            }
+            else if(i+j < k){
+                i = l.next();
+            }
+            else{
+                j = r.next();
             }
         }
         return flag;

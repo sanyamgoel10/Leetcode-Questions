1b46d6c9d1839a228bc1f9cb40b1d738c2ab27bd
Sanyam Goel <57743195+sanyamgoel10@users.noreply.github.com>
2022-09-15 00:22:23 +0530

    Time: 57 ms (35.48%), Space: 24.9 MB (83.42%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+        // dry run for k=3, nums = [1,2,3,4,5,6,7]
+        // 1 2 3 4 5 6 7
+        // 7 6 5 4 3 2 1
+        // 5 6 7 4 3 2 1
+        // 5 6 7 1 2 3 4
+        
         int n = nums.size();
-        if ((n == 0) || (k <= 0))
-            {
-                return;
-            }
-            
-            // Make a copy of nums
-            vector<int> numsCopy(n);
-            for (int i = 0; i < n; i++)
-            {
-                numsCopy[i] = nums[i];
-            }
-            
-            // Rotate the elements.
-            for (int i = 0; i < n; i++)
-            {
-                nums[(i + k)%n] = numsCopy[i];
-            }
+        k = k % n;  //for very large k
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };

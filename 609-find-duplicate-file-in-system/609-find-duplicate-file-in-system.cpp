b9c91f050e17a95ea922dc0ee358ba2edaacf8fb
Sanyam Goel <57743195+sanyamgoel10@users.noreply.github.com>
2022-09-19 23:28:30 +0530

    Time: 232 ms (27.10%), Space: 39.6 MB (58.01%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
@@ -1,53 +1,52 @@
 class Solution {
+    int solve(int& start, string& dir, string& file, string& content, string& s){
+        int l = start;
+        int r = s.size();
+        
+        //getting current directory
+        while(l<r && s[l]!=' '){
+            dir += s[l];
+            l++;
+        }
+        l++;
+        
+        //getting current file
+        while(l<r && s[l]!='('){
+            file += s[l];
+            l++;
+        }
+        l++;
+        
+        //getting content value
+        while(l<r && s[l]!=')'){
+            content += s[l];
+            l++;
+        }
+        
+        start = l;  //updating start's location to get file and contents of same directory if present
+        return l;
+    }
 public:
     vector<vector<string>> findDuplicate(vector<string>& paths) {
         int n = paths.size();
         vector<vector<string>> ans;
-        map<string, vector<string>> hmap;
         
-        for(auto address : paths){
-            int ind = 0;
-            while(ind<address.size() && address[ind]!=' '){
-                ind++;
-            }
-            string dir = address.substr(0, ind);
-            dir = dir + "/";
-            // cout<<dir<<" : ";
+        unordered_map<string, vector<string>> hmap;
+        
+        for(int i=0;i<n;i++){
+            string curr = paths[i];
             
-            int cnt = ind+1;
-            for(int i=ind+1; i<address.size(); i++){
-                if(address[i]==' '){
-                    string temp = address.substr(cnt, i-cnt);
-                    // cout<<temp<<",";
-                    
-                    int l = temp.find('('); //find (
-                    int r = temp.size()-1;  //find )
-                    string content = temp.substr(l+1, r-l+1);
-                    string file = temp.substr(0,l);
-                    hmap[content].push_back(dir+file);
-                    
-                    cnt = i+1;
-                }
+            string content = "";
+            string file = "";
+            string dir = "";
+            int start = 0;
+            while(solve(start, dir, file, content, curr) <= curr.size()){
+                hmap[content].push_back(dir + "/" + file);
+                content = "";
+                file = "";
+                start++;
             }
-            string temp = address.substr(cnt, address.size()-cnt);
-            // cout<<temp<<",";
-            int l = temp.find('('); //find (
-            int r = temp.size()-1;  //find )
-            string content = temp.substr(l+1, r-l+1);
-            string file = temp.substr(0,l);
-            hmap[content].push_back(dir+file);
-            
-            // cout<<endl;
         }
-        
-        // for(auto x : hmap){
-        //     cout<<x.first<<" : ";
-        //     for(auto y : x.second){
-        //         cout<<y<<",";
-        //     }
-        //     cout<<endl;
-        // }
-
         
         for(auto x : hmap){
             if(x.second.size()>1){

d116b7ce12593659f210497aa476848cc7ecc337
Sanyam Goel <57743195+sanyamgoel10@users.noreply.github.com>
2022-09-20 20:00:34 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (40.43%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,18 +1,4 @@
 class Solution {
-    int solve(int n, vector<int>& dp){
-        if(n<=1){
-            return 1;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        
-        int ans = 0;
-        for(int i=1;i<=n;i++){
-            ans += solve(i-1, dp)*solve(n-i, dp);
-        }
-        return dp[n] = ans;
-    }
 public:
     int numTrees(int n) {
         /*
@@ -23,7 +9,17 @@ public:
         
         fn -> f0xf(n-1) + f1xf(n-2) + f2xf(n-3) + ...... + f(n-3)xf2 + f(n-2)xf1 + f(n-1)xf0
         */
-        vector<int> dp(n+1, -1);
-        return solve(n, dp);
+        
+        vector<int> dp(n+1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int ind=2;ind<=n;ind++){
+            for(int i=1;i<=ind;i++){
+                dp[ind] += dp[i-1]*dp[ind-i];
+            }
+        }
+        
+        return dp[n];
     }
 };

c9037a3002761adbc4a79af16e13745fd3039451
Sanyam Goel <57743195+sanyamgoel10@users.noreply.github.com>
2022-09-24 00:23:10 +0530

    Time: 31 ms (53.12%), Space: 8.4 MB (61.03%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -3,16 +3,21 @@ public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
         
+        int l = 0;
+        int r = 0;
         int ans = 0;
         
-        for(int i=0;i<n;i++){
-            unordered_map<char,int> hmap;
-            int j = i;
-            while(hmap.count(s[j])==false && j<n){
-                hmap[s[j]]++;
-                j++;
+        unordered_map<char,int> hmap;
+        
+        while(r<n){
+            hmap[s[r]]++;
+            while(hmap[s[r]] > 1){
+                hmap[s[l]]--;
+                l++;
             }
-            ans = max(ans, j-i);
+            
+            ans = max(ans, r-l+1);
+            r++;
         }
         
         return ans;

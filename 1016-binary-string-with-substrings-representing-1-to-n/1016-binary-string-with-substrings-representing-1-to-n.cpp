2894227676aebcf96ec0edc6ecf66e85369e8f0c
Sanyam Goel <57743195+sanyamgoel10@users.noreply.github.com>
2022-09-27 09:49:11 +0530

    Time: 4 ms (59.39%), Space: 6.3 MB (90.42%) - LeetHub

--- 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.cpp
+++ 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.cpp
@@ -8,21 +8,22 @@ class Solution {
         return ans;
     }
     bool isSubstr(string& s, string& q){
-        int m = s.size();
-        int n = q.size();
-        int flag = 0;
-        for(int i=0;i<m-n+1;i++){
-            if(s.substr(i, n) == q){
-                flag = 1;
-            }
-        }
-        return flag==1;
+        // int m = s.size();
+        // int n = q.size();
+        // int flag = 0;
+        // for(int i=0;i<m-n+1;i++){
+        //     if(s.substr(i, n) == q){
+        //         flag = 1;
+        //     }
+        // }
+        // return flag==1;
+        return s.find(q) != -1;
     }
 public:
     bool queryString(string s, int n) {
         for(int i=1;i<=n;i++){
             string bin = getBinary(i);
-            cout<<bin<<" ";
+            // cout<<bin<<" ";
             if(!isSubstr(s, bin)){
                 return false;
             }

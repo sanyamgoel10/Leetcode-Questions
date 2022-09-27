65905b7fa16c8f365720e064c52cb9208c431d71
Sanyam Goel <57743195+sanyamgoel10@users.noreply.github.com>
2022-09-27 09:40:58 +0530

    Time: 45 ms (12.64%), Space: 6.4 MB (49.43%) - LeetHub

---
+++ 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    string getBinary(int i){
+        string ans = "";
+        while(i>0){
+            ans.insert(0, to_string(i%2));
+            i /= 2;
+        }
+        return ans;
+    }
+    bool isSubstr(string& s, string& q){
+        int m = s.size();
+        int n = q.size();
+        int flag = 0;
+        for(int i=0;i<m-n+1;i++){
+            if(s.substr(i, n) == q){
+                flag = 1;
+            }
+        }
+        return flag==1;
+    }
+public:
+    bool queryString(string s, int n) {
+        for(int i=1;i<=n;i++){
+            string bin = getBinary(i);
+            cout<<bin<<" ";
+            if(!isSubstr(s, bin)){
+                return false;
+            }
+        }
+        return true;
+    }
+};

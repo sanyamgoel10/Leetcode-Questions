5e35ba8b0ab8c366eec2ec797277cf379bfe4d14
Sanyam Goel <57743195+sanyamgoel10@users.noreply.github.com>
2022-09-28 12:59:26 +0530

    Time: 3 ms (36.74%), Space: 6 MB (31.85%) - LeetHub

--- 1227-airplane-seat-assignment-probability/1227-airplane-seat-assignment-probability.cpp
+++ 1227-airplane-seat-assignment-probability/1227-airplane-seat-assignment-probability.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     double nthPersonGetsNthSeat(int n) {
-        //by mathematical induction, answer will always be 0.5
-        if(n>=2){
-            return (double)1/(double)2;
+        //by mathematical induction, answer will always be 0.5.
+        
+        /*
+            After the first person, neither of the passengers show any preference for the last person's seat nor the seat of the first passenger.
+
+Once all the passengers except the last passenger occupy the seats, the first passenger would be sitting in the seat allotted to him or in that of the last person.
+
+Therefore, the probability that the last person to board gets his assigned seat unoccupied is
+
+        */
+        
+        if(n<2){
+            return 1;
         }
-        return 1;
+        return (double)1/(double)2;
     }
 };

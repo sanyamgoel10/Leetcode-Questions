81fba4aacf0fd9a7e1475a9b1f71b3f20deab517
Sanyam Goel <57743195+sanyamgoel10@users.noreply.github.com>
2022-10-06 18:04:41 +0530

    Time: 715 ms (28.28%), Space: 128.1 MB (70.01%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -1,22 +1,54 @@
 class TimeMap {
-    unordered_map<int, unordered_map<string,string>> hmap;
+    // {key, {{timestamp1, value1}, {timestamp2, value2}, {timestamp3, value3}}}
+    unordered_map<string, vector<pair<int, string>>> hmap;
 public:
     TimeMap() {
         
     }
     
     void set(string key, string value, int timestamp) {
-        hmap[timestamp][key] = value;
+        hmap[key].push_back({timestamp, value});
     }
     
     string get(string key, int timestamp) {
-        while(timestamp>0){
-            if(hmap.count(timestamp) && hmap[timestamp].count(key)){
-                return hmap[timestamp][key];
-            }
-            timestamp--;
+        if(!hmap.count(key)){
+            //key not present in the hmap
+            return "";
         }
-        return "";
+        //using binary search to find out the timestamp just smaller than input timestamp
+        int l = 0;
+        int r = hmap[key].size()-1;
+        
+        if(hmap[key][l].first > timestamp){
+            // the timestamps inside vector are greatetr than input
+            return "";
+        }
+        if(hmap[key][r].first <= timestamp){
+            //last timestamp of vector is smaller than input
+            return hmap[key][r].second;
+        }
+        
+        while(l<r-1){
+            int mid = (l+r)/2;
+            
+            if(hmap[key][mid].first < timestamp){
+                //goto right half of vector
+                l = mid;
+            }
+            else if(hmap[key][mid].first == timestamp){
+                return hmap[key][mid].second;
+            }
+            else{
+                r = mid;
+            }
+        }
+        
+        if(hmap[key][r].first <= timestamp){
+            return hmap[key][r].second;
+        }
+        else{
+            return hmap[key][l].second;
+        }
     }
 };
 

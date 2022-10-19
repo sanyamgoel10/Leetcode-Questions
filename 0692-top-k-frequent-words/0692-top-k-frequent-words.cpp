61c200c57ae4f007bdf4996a68ffe88eafea45d0
Sanyam Goel <57743195+sanyamgoel10@users.noreply.github.com>
2022-10-19 23:49:36 +0530

    Time: 30 ms (34.28%), Space: 12.8 MB (18.22%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -5,31 +5,23 @@ class Solution {
         }
         return a.second>b.second;
     }
-    void sortMap(map<string, int>& hmap, int& k, vector<string>& ans){
-        vector<pair<string,int>> temp;
-        for(auto& x : hmap){
-            temp.push_back(x);
-        }
-        sort(temp.begin(), temp.end(), cmp);
-        
-        for(int i=0;i<k;i++){
-            ans.push_back(temp[i].first);
-        }
-    }
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
-        map<string, int> hmap;
+        unordered_map<string, int> hmap;
         for(auto x : words){
             hmap[x]++;
         }
+        vector<pair<string,int>> temp;
+        for(auto x : hmap){
+            temp.push_back(x);
+        }
+        
+        sort(temp.begin(), temp.end(), cmp);
         
         vector<string> ans;
-        
-        sortMap(hmap, k, ans);
-        
-        // for(auto x :  hmap){
-        //     cout<<x.first<<" "<<x.second<<endl;
-        // }
+        for(int i=0;i<k;i++){
+            ans.push_back(temp[i].first);
+        }
         
         return ans;
     }

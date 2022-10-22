3b4d562725a67f8333327798561c877f406bef7c
Sanyam Goel <57743195+sanyamgoel10@users.noreply.github.com>
2022-10-22 20:00:28 +0530

    Time: 166 ms (97.04%), Space: 88.6 MB (9.61%) - LeetHub

--- 2012-sum-of-beauty-in-the-array/2012-sum-of-beauty-in-the-array.cpp
+++ 2012-sum-of-beauty-in-the-array/2012-sum-of-beauty-in-the-array.cpp
@@ -4,26 +4,26 @@ public:
         int n = nums.size();
         int ans = 0;
         
-        vector<int> maxTillNow(n);
-        vector<int> minTillEnd(n);
+        vector<int> maxTillNow(n);  //prefix maximum array
+        vector<int> minTillEnd(n);  //suffix minimum array
         
-        int maxi = nums[0];     //maximum in prefix array
-        int mini = nums[n-1];     //minimum in suffix array
+        maxTillNow[0] = nums[0];     //maximum in prefix array
+        minTillEnd[n-1] = nums[n-1];     //minimum in suffix array
         for(int i=1;i<n-1;i++){ 
-            maxTillNow[i] = maxi;
-            maxi = max(maxi, nums[i]);
+            // maxTillNow[i] = maxi;
+            maxTillNow[i] = max(maxTillNow[i-1], nums[i]);
         }
         for(int i=n-2;i>0;i--){
-            minTillEnd[i] = mini;
-            mini = min(mini, nums[i]);
+            // minTillEnd[i] = mini;
+            minTillEnd[i] = min(minTillEnd[i+1], nums[i]);
         }
         
         for(int i=1;i<n-1;i++){
-            if(maxTillNow[i]<nums[i] && nums[i]<minTillEnd[i]){
+            if(maxTillNow[i-1]<nums[i] && nums[i]<minTillEnd[i+1]){
                 ans += 2;
             }
             else if(nums[i-1]<nums[i] && nums[i]<nums[i+1]){
-                ans += 1;
+                ans++;
             }
         }
         

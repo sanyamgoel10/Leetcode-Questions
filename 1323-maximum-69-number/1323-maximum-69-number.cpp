dc9e9708c24b8dc1abbc44ec4e3e4236e1e23c15
Sanyam Goel <57743195+sanyamgoel10@users.noreply.github.com>
2022-11-07 23:08:40 +0530

    Time: 3 ms (38.22%), Space: 6 MB (45.85%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string s = to_string(num);
+        for(auto& x : s){
+            if(x=='6'){
+                x='9';
+                break;
+            }
+        }
+        return stoi(s);
+    }
+};
